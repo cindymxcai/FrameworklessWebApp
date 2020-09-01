87fb260b4a4888c254ae8b9002a512b445d570e0
Cindy Cai <cindymxcai@gmail.com>
2020-09-02 11:45:59 +1200

    first server

---
+++ FrameworklessKata/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+
+namespace FrameworklessKata
+{
+    internal static class Program
+    {
+       public static void Main(string[] args)
+        {
+            var server = new HttpListener();
+            server.Prefixes.Add("http://localhost:8080/");
+            server.Start();
+            while (true)
+            {
+                var context = server.GetContext(); // Gets the request
+                Console.WriteLine($"{context.Request.HttpMethod} {context.Request.Url}");
+                var buffer = System.Text.Encoding.UTF8.GetBytes("Hello");
+                context.Response.ContentLength64 = buffer.Length;
+                context.Response.OutputStream.Write(buffer, 0, buffer.Length); // forces send of response
+            }
+
+           // server.Stop(); never reached...        
+        }
+    }
+}
