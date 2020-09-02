7dc4a569b0da37c4c8056e9ab278601195762a2e
Cindy Cai <cindymxcai@gmail.com>
2020-09-02 17:23:05 +1200

    switch handling get and post methods

--- FrameworklessKata/Request.cs
+++ FrameworklessKata/Request.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using System.Net;
 
 namespace FrameworklessKata
@@ -10,6 +12,21 @@ namespace FrameworklessKata
             {
                 case "/":
                     Response.Write("Index",context);
+                    break;
+                case "/ids":
+                    switch (context.Request.HttpMethod)
+                    {
+                        case "GET":
+                            Console.WriteLine("getting IDs");
+                            Response.Write("Returned IDs", context);
+                            break;
+                        case "POST":
+                            Console.WriteLine("posting to IDs");
+                            var body = context.Request.InputStream;
+                            var reader = new StreamReader(body, context.Request.ContentEncoding);
+                            Response.Write(reader.ReadToEnd(), context);
+                            break;
+                    }
                     break;
                 default:
                     Response.Write("404", context);   
